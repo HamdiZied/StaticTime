9abc38266bed84b88bdd5d6a88b7e143396602be
92959366ZIED <hhamdiziyed990@gmail.com>
2019-07-08 12:09:53 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Timer.css';
+import Timer from  './Timer';
+
+function App() {
+  return (
+    <div className="Timer">
+    <Timer />
+  </div>
+  );
+}
+
+export default App;
---
+++ src/Timer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+const Timer=()=>{
+    return (
+        <div className="Date">
+             <div className="hours">
+                 <div className="Number-hours">
+                    <h1>00</h1>
+                    <h3>Hours</h3>
+                 </div>
+             </div>
+
+             <div className="minute">
+                 <div className="Number-second">
+                    <h1>00</h1>
+                    <h3>Minutes</h3>
+                 </div>
+             </div>
+
+             <div className="second">
+                 <div className="Number-second">
+                    <h1>00</h1>
+                    <h3>Second</h3>
+                 </div>
+             </div>
+
+           
+           
+        </div>
+    )
+         
+    } 
+    
+export default Timer;
